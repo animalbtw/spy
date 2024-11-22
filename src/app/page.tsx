c6244fdea980d4f5e2a121b30f474407798e8830
animalbtw <animalbtw@gmail.com>
2024-11-22 16:42:18 +0300

    visualization fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,18 @@ export default async function Home() {
     const ip = data.get('x-forwarded-for')
     const reqUrl = data.get('request-url')
 
-    console.log(ip)
-    console.log(reqUrl)
-
     return (
       <div className={styles.page}>
         <div className={styles.container}>
           <div>
             loading or something is wrong
           </div>
-          <div className={styles.ip}>
+          {/* <div className={styles.ip}>
             {ip}
           </div>
           <div>
             {reqUrl}
-          </div>
+          </div> */}
         </div>
       </div>
     );
