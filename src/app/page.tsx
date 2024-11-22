897c67ab4c97dc94a06e2f8928c7b1db1db89aef
animalbtw <animalbtw@gmail.com>
2024-11-22 16:18:21 +0300

    visualization fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,25 @@
-import {headers} from "next/headers";
 import styles from "./page.module.css";
 import { getClientData } from "./shared/api/reqs";
 
 export default async function Home() {
-  let data = await getClientData().then(res => res)
+  const data = await getClientData().then(res => res)
 
   if (data) {
     const serData = new Array(data)
     return (
       <div className={styles.page}>
-        {serData.map((val) => (
-          <div>
+        {serData.map((val, index) => (
+          <div key={index}>
             {val}
           </div>
         ))}
       </div>
     )
   }
-  console.log(data)
+
   return (
     <div className={styles.page}>
-      {}
+      loading or smth is wrong
     </div>
   );
 }
