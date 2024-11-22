b6a16fae581aa622edcec2e1c0ff445971bbc9f8
animalbtw <animalbtw@gmail.com>
2024-11-22 16:01:03 +0300

    init

---
+++ src/app/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+function middleware(req: any) {
+    const requestHeaders = new Headers(req.headers)
+  
+    // Add new request headers
+    requestHeaders.set('request-ip', req.ip);
+    requestHeaders.set('request-url', req.url);
+    requestHeaders.set('request-geo', JSON.stringify(req.geo));
+  
+    return NextResponse.next({
+      request: {
+        headers: requestHeaders,
+      },
+    })
+  }
